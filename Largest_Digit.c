4b5a6b191f8f162882e2c98577339ffe53c0c59a
sethukumar31 <97594579+sethukumar31@users.noreply.github.com>
2022-05-02 11:28:53 +0530

    #include<stdio.h> int main() {     int n,m,r,arr[100],k=0,i,max=0;     scanf("%d",&n);     while(n>0)     {         r=n%10;         arr[k]=r;         k++;         n=n/10;     }      max=arr[0];     for(i=0;i<=k;i++)     {         //max=arr[0];         if(arr[i]>max)         {             max=arr[i];         }     }     printf("%d",max);     return 0; }

---
+++ Largest_Digit.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int n,m,r,arr[100],k=0,i,max=0;
+    scanf("%d",&n);
+    while(n>0)
+    {
+        r=n%10;
+        arr[k]=r;
+        k++;
+        n=n/10;
+    }
+     max=arr[0];
+    for(i=0;i<=k;i++)
+    { 
+        //max=arr[0];
+        if(arr[i]>max)
+        {
+            max=arr[i];
+        }
+    }
+    printf("%d",max);
+    return 0;
+}
