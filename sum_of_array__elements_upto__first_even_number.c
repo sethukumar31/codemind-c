7d348bc450decf9c3018fafb7302f5093108e568
sethu kumar kongu <97594579+sethukumar31@users.noreply.github.com>
2022-05-31 23:46:35 +0530

    #include<stdio.h> int main() {     int n,arr[10000],i,sum,k;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         if(arr[i]%2!=0)         {             sum+=arr[i];         }         else         {         break;         }     }     printf("%d",sum);
    }

---
+++ sum_of_array__elements_upto__first_even_number.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int n,arr[10000],i,sum,k;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]%2!=0)
+        {
+            sum+=arr[i];
+        }
+        else
+        {
+        break;
+        }
+    }
+    printf("%d",sum);
+    
+}
