38711c3f7279243c9f0969d9bf1b85efdf951c85
sethu kumar kongu <97594579+sethukumar31@users.noreply.github.com>
2022-06-03 23:41:21 +0530

    #include<stdio.h> int isprime(int i) {     int m,c=0,j;     for(j=1;j<=i;j++)     {         if(i%j==0)         {             c++;         }     }         if(c==2)         {             return 1;         }         else         {             return 0;         }
    }
    int main()
    {
        int n,arr[1000],i,p=0,res,k;
        scanf("%d",&n);
        for(i=0;i<n;i++)
        {
            scanf("%d",&arr[i]);
        }
        scanf("%d",&k);
        for(i=0;i<n;i++)
        {
            if(arr[i]<=k)
            {
             res=isprime(arr[i]);
             if(res==1)
              {
              p++;
              }
            }
        }
        printf("%d",p);
        return 0;
    }

---
+++ primes_less_than_k.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+int isprime(int i)
+{
+    int m,c=0,j;
+    for(j=1;j<=i;j++)
+    {
+        if(i%j==0)
+        {
+            c++;
+        }
+    }
+        if(c==2)
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+        
+}
+int main()
+{
+    int n,arr[1000],i,p=0,res,k;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    scanf("%d",&k);
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]<=k)
+        {
+         res=isprime(arr[i]);
+         if(res==1)
+          {
+          p++;
+          }
+        }
+    }
+    printf("%d",p);
+    return 0;
+}
