11e9ae76a9cdc95929d3c010ce430d3459ecb78a
sethu kumar kongu <97594579+sethukumar31@users.noreply.github.com>
2022-07-16 11:26:06 +0530

    #include<stdio.h> #include<math.h> int main() {     double p,t,r,cp,x,y;     scanf("%lf%lf%lf",&p,&r,&t);     x=1+(r/100);     y=pow(x,t);     cp=p*y;     printf("%.2lf",cp); }

--- Compound_interest.c
+++ Compound_interest.c
@@ -2,12 +2,10 @@
 #include<math.h>
 int main()
 {
-    double p,r,t,n,m=0,q=0,l=0,cp;
-    scanf("%lf %lf %lf",&p,&r,&t);
-    m=(1+(r/100));
-   // l=n*t;
-    q=pow(m,t);
-    cp=p*q;
+    double p,t,r,cp,x,y;
+    scanf("%lf%lf%lf",&p,&r,&t);
+    x=1+(r/100);
+    y=pow(x,t);
+    cp=p*y;
     printf("%.2lf",cp);
-    return 0;
 }
