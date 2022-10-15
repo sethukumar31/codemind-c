8b729e401254a371ae7777598b20b3a0f958bc5d
sethu kumar kongu <97594579+sethukumar31@users.noreply.github.com>
2022-10-16 01:08:17 +0530

    #include<stdio.h> int main() {     int n,arr[100],i,j,k;     scanf("%d",&n);     for(i=0;i<n;i++)     scanf("%d",&arr[i]);     for(i=0;i<n;i++)     {         for(j=i+1;j<n;j++)         {             if(arr[i]==arr[j])             {                 for(k=j;k<n-1;k++)                 {                     arr[k]=arr[k+1];                 }                 n--;                 j--;             }         }     }     for(i=0;i<n;i++)     {         printf("%d ",arr[i]);     }     return 0; }

--- unique_elements.c
+++ unique_elements.c
@@ -1,34 +1,28 @@
 #include<stdio.h>
 int main()
 {
-    int n,arr[2000],i,j,a,b,m=0,c=0;
+    int n,arr[100],i,j,k;
     scanf("%d",&n);
     for(i=0;i<n;i++)
     scanf("%d",&arr[i]);
-    //scanf("%d %d",&a,&b);
-     //max=0;
     for(i=0;i<n;i++)
     {
-        c=1;
-       // printf("%d ",arr[i]);
-        for(j=0;j<n;j++)
+        for(j=i+1;j<n;j++)
         {
-           if(i!=j && arr[i]==arr[j])
-           {
-              c++;
-              arr[j]=-1;
-           }
+            if(arr[i]==arr[j])
+            {
+                for(k=j;k<n-1;k++)
+                {
+                    arr[k]=arr[k+1];
+                }
+                n--;
+                j--;
+            }
         }
-    
-    if(c>=1 && arr[i]!=-1)
-    {
-      printf("%d ",arr[i]);
-      m++;
     }
-    }          
-  if(m==0)
-  {
-      printf("-1");
-  }
+    for(i=0;i<n;i++)
+    {
+        printf("%d ",arr[i]);
+    }
     return 0;
 }
