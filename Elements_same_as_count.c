01cf9d82f110fe462d26a706a431a56ffa7b7dc4
sethu kumar kongu <97594579+sethukumar31@users.noreply.github.com>
2022-10-16 01:24:39 +0530

    #include<stdio.h> int main() {     int n,arr[10000],i,j,d=0,c=0,flag=0,k=0;     float avg;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         c=1;         for(j=0;j<n;j++)         {
                if(arr[i]==arr[j] && i!=j && arr[i]>0)
                {
                    c++;
                    arr[j]=-1;
                    
                }
            }
            if(c==arr[i])
            {
                flag=1;
              printf("%d ",arr[i]);
               
            }
            
            
        }
        if(flag==0)
        printf("-1");
        
        
    }

--- Elements_same_as_count.c
+++ Elements_same_as_count.c
@@ -1,29 +1,37 @@
 #include<stdio.h>
 int main()
 {
-    int n,arr[100],i,j,c=0,m=0;
+    int n,arr[10000],i,j,d=0,c=0,flag=0,k=0;
+    float avg;
     scanf("%d",&n);
     for(i=0;i<n;i++)
-    scanf("%d",&arr[i]);
+    {
+        scanf("%d",&arr[i]);
+    }
     for(i=0;i<n;i++)
     {
         c=1;
         for(j=0;j<n;j++)
         {
-            if(i!=j && arr[i]==arr[j])
+            
+            if(arr[i]==arr[j] && i!=j && arr[i]>0)
             {
                 c++;
                 arr[j]=-1;
+                
             }
         }
         if(c==arr[i])
         {
-            m++;
-            printf("%d ",arr[i]);
+            flag=1;
+          printf("%d ",arr[i]);
+           
         }
+        
+        
     }
-    if(m==0)
-    {
-        printf("-1");
-    }
+    if(flag==0)
+    printf("-1");
+    
+    
 }
